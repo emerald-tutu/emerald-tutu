76c0eb17c11ccfae311be2c03c2feb48ad326dc1
Luke Novak <contact@nluken.com>
2023-04-24 23:50:40 -0400

    added photo and border back to person

--- components/blocks/person.js
+++ components/blocks/person.js
@@ -2,27 +2,30 @@ import Circle from '../circle'
 
 export default function Person({name, title, bio, photo}) {
     return (
-        <div className="p-0 my-3">
-            <div className="mb-3">
-                {/* photo currently unused. find out if we still need this
-                photo && (
-                    <div className="col-4 p-0 person-mh blue-border d-flex">
-                        <img
-                            className="mw-100 mh-100"
-                            src={photo.url}
-                            alt={name}
-                        />
+        <div className="p-3 my-3 blue-border d-flex">
+            <div className='me-4'>
+                <div className="mb-3">
+
+                    <div className="d-flex font-monument mb-3">
+                        <Circle size="22"/>
+                        <h3 className='ms-3 my-auto fw-bold'>{name}, {title}</h3>
                     </div>
-                ) */}
-                <div className="d-flex font-monument mb-3">
-                    <Circle size="22"/>
-                    <h3 className='ms-3 my-auto fw-bold'>{name}, {title}</h3>
+                </div>
+                <div className='mx-auto'>
+                    <p>{bio}</p>
                 </div>
             </div>
-            <div className='mx-auto'>
-                <p>{bio}</p>
-            </div>
 
+            {
+            photo && (
+                <div className="col-4 p-0 person-mh my-auto d-flex">
+                    <img
+                        className="mw-100 mh-100"
+                        src={photo.url}
+                        alt={name}
+                    />
+                </div>
+            )}
         </div>
         
     )
