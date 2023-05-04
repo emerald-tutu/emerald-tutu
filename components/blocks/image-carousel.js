0543418706a6af39de4e4105e293785f86a6d541
Luke Novak <contact@nluken.com>
2023-05-03 22:03:36 -0400

    added white backgrounds for carousels

--- components/blocks/image-carousel.js
+++ components/blocks/image-carousel.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Image from "./image";
 
-export default function ImageCarousel({images, id}) {
+export default function ImageCarousel({images, blueBackground, id}) {
     const [active, setActive] = useState(0);
     const length = images.length;
 
@@ -25,7 +25,7 @@ export default function ImageCarousel({images, id}) {
     }
 
     return (
-        <div id={id} className="width-max-content w-100 my-auto mx-0 pt-4 pb-2 mb-2 blue-bg d-flex flex-column">
+        <div id={id} className={`width-max-content w-100 my-auto mx-0 pt-4 pb-2 mb-4 d-flex flex-column ${blueBackground? "blue-bg" : ""}`}>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,200" />
             <div className="mx-auto mt-auto">
                 {images.map((img, idx) => 
@@ -39,7 +39,7 @@ export default function ImageCarousel({images, id}) {
                     </div>)}
                 <div className="d-flex w-100">
                 <div className="carousel-arrow me-auto d-flex" onClick={prevSlide}>
-                    <span className="material-symbols-outlined my-auto" style={{fontSize:"48px", fontWeight:"700"}}>
+                    <span className={`material-symbols-outlined my-auto ${!blueBackground? "color-blue" : ""}`} style={{fontSize:"48px", fontWeight:"700"}}>
                     arrow_back
                     </span>
                 </div>
@@ -47,13 +47,13 @@ export default function ImageCarousel({images, id}) {
                     <div className="my-auto mx-auto d-flex">
                         {images.map((img, idx) => 
                             <div key={idx} onClick={goToSlide}>
-                                {idx === active && (<div data-index={idx} className="circle bg-green"/>)}
-                                {idx !== active && (<div data-index={idx} className="circle bg-white"/>)}
+                                {idx === active && (<div data-index={idx} className={`circle ${blueBackground? "bg-green" : "blue-bg"}`}/>)}
+                                {idx !== active && (<div data-index={idx} className={`circle ${blueBackground? "bg-white" : "border-thin-blue"}`}/>)}
                             </div>)}
                     </div>
                 </div>
                 <div className="carousel-arrow ms-auto d-flex" onClick={nextSlide}>
-                    <span className="material-symbols-outlined my-auto" style={{fontSize:"48px", fontWeight:"700"}}>
+                    <span className={`material-symbols-outlined my-auto ${!blueBackground? "color-blue" : ""}`} style={{fontSize:"48px", fontWeight:"700"}}>
                     arrow_forward
                     </span>
                 </div>
